10ffbdf68af9c6fa4e224e2b2535e29aeab5c9a9
Xuemin Guan <gxuemin@gmail.com>
2022-02-03 18:00:26 +0000

    XG creat posts

---
+++ app/routes/admin/new.tsx
@@ -0,0 +1,53 @@
+import {redirect, Form} from "remix";
+import {createPost} from "~/post";
+import type {ActionFunction} from "remix";
+
+
+export const action: ActionFunction = async ({
+                                                 request
+                                             }) => {
+    const formData = await request.formData();
+
+    const title = formData.get("title");
+    const slug = formData.get("slug");
+    const markdown = formData.get("markdown");
+
+    if (Object.keys(errors).length) {
+        return errors;
+    }
+
+    const errors = {};
+    if (!title) errors.title = true;
+    if (!slug) errors.slug = true;
+    if (!markdown) errors.markdown = true;
+
+    await createPost({title, slug, markdown});
+
+    return redirect("/admin");
+};
+
+
+export default function NewPost() {
+    return (
+        <Form method="post">
+            <p>
+                <label>
+                    Post Title: <input type="text" name="title"/>
+                </label>
+            </p>
+            <p>
+                <label>
+                    Post Slug: <input type="text" name="slug"/>
+                </label>
+            </p>
+            <p>
+                <label htmlFor="markdown">Markdown:</label>
+                <br/>
+                <textarea id="markdown" rows={20} name="markdown"/>
+            </p>
+            <p>
+                <button type="submit">Create Post</button>
+            </p>
+        </Form>
+    );
+}
