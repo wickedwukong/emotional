b79c54ca457e96f26dd193812e864ca37255c712
Xuemin Guan <gxuemin@gmail.com>
2022-02-03 18:07:08 +0000

    add type and invariant check on errors

--- app/routes/admin/new.tsx
+++ app/routes/admin/new.tsx
@@ -2,6 +2,13 @@ import {useActionData, Form, redirect} from "remix";
 import {createPost} from "~/post";
 import type {ActionFunction} from "remix";
 
+import invariant from "tiny-invariant";
+
+type PostError = {
+    title?: boolean;
+    slug?: boolean;
+    markdown?: boolean;
+};
 
 export const action: ActionFunction = async ({
                                                  request
@@ -12,7 +19,7 @@ export const action: ActionFunction = async ({
     const slug = formData.get("slug");
     const markdown = formData.get("markdown");
 
-    const errors = {};
+    const errors: PostError = {};
     if (!title) errors.title = true;
     if (!slug) errors.slug = true;
     if (!markdown) errors.markdown = true;
@@ -21,6 +28,9 @@ export const action: ActionFunction = async ({
         return errors;
     }
 
+    invariant(typeof title === "string");
+    invariant(typeof slug === "string");
+    invariant(typeof markdown === "string");
 
     await createPost({title, slug, markdown});
 
